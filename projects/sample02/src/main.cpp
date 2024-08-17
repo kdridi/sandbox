687af16963d50c8a7a8230d90c774a8c5b8be961
Karim DRIDI <karim.dridi@gmail.com>
2024-08-18 00:29:48 +0200

    add projects/sample02: use SFML with vcpkg

---
+++ projects/sample02/src/main.cpp
@@ -0,0 +1,30 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+            if (event.type == sf::Event::KeyPressed)
+            {
+                if (event.key.code == sf::Keyboard::Escape)
+                    window.close();
+            }
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
