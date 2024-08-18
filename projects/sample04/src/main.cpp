fdbd208978f67ad3a6efccfc116cf2c443f855c2
Karim DRIDI <karim.dridi@gmail.com>
2024-08-18 03:00:32 +0200

    add projects/sample04: copy assets dir next to executable then run

---
+++ projects/sample04/src/main.cpp
@@ -0,0 +1,25 @@
+#include <fmt/core.h>
+
+#include <filesystem>
+#include <fstream>
+
+int main(int argc, const char *argv[])
+{
+    std::filesystem::path execPath = std::filesystem::absolute(argv[0]).parent_path();
+    fmt::print("Executable path: {}\n", execPath.string());
+
+    std::string filePath = (execPath / "assets/sample.txt").string();
+    fmt::print("File path: {}\n", filePath);
+
+    std::ifstream file(filePath);
+    if (!file.is_open())
+    {
+        fmt::print("Error while opening file '{}'\n", filePath);
+        return EXIT_FAILURE;
+    }
+
+    std::string fileContent((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    fmt::print("File content: {}\n", fileContent);
+
+    return EXIT_SUCCESS;
+}
